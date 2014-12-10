f0106a31e9d165f83a9f3ab00f34048ad2d2b2db
StephanBijzitter <stephanbijzitter@gmail.com>
2014-12-10 10:38:30 +0100

    Fixed typo

--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -7,7 +7,7 @@ require.config({
         requirejs : '../lib/requirejs/require',
         almond : '../lib/almond/almond',
         'famous-material' : '../lib/famous-material/src',
-        'famous-flex' : '..lib/famous-flex/src'
+        'famous-flex' : '../lib/famous-flex/src'
     },
     packages : [
 
