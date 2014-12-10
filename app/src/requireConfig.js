fc7e38903e103a30bf68d101447975bc61361d52
StephanBijzitter <stephanbijzitter@gmail.com>
2014-12-10 10:36:12 +0100

    Added Famous Flex

--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -6,7 +6,8 @@ require.config({
         famous : '../lib/famous/src',
         requirejs : '../lib/requirejs/require',
         almond : '../lib/almond/almond',
-        'famous-material' : '../lib/famous-material/src'
+        'famous-material' : '../lib/famous-material/src',
+        'famous-flex' : '..lib/famous-flex/src'
     },
     packages : [
 
