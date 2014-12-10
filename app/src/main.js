fd800026772bd71e2dc6b3c22cc72ef35afad193
StephanBijzitter <stephanbijzitter@gmail.com>
2014-12-10 11:07:22 +0100

    Added search in footer

--- app/src/main.js
+++ app/src/main.js
@@ -6,15 +6,20 @@ define(function(require, exports, module) {
     var TouchEffect = require('famous-material/mixins/TouchEffect');
     var ButtonEffect = require('famous-material/mixins/ButtonEffect');
 
+    // import famous flex
+    var LayoutController = require('famous-flex/LayoutController');
+    var NavBarLayout = require('famous-flex/layouts/NavBarLayout');
+
     // import vanilla famo.us
     var Engine = require('famous/core/Engine');
     var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
+    var InputSurface = require('famous/surfaces/InputSurface');
 
     // start creating components
     var context = Engine.createContext();
 
-    // create the header with z-index 20
+    // create the header
     var header = new Paper({
         depth : 20,
         size : [undefined, 48],
@@ -25,14 +30,49 @@ define(function(require, exports, module) {
         }
     });
 
-    // create the footer with z-index 20
-    var footer = new Paper({
+    // create a search button for the footer
+    var search_button = new Paper({
         depth : 20,
-        size : [undefined, 32],
+        size : [64],
         type : Paper.TYPE_TILE,
-        content : 'Famous Material Examples',
-        attributes : {
-            id : 'skelware-footer'
+        content : 'Search',
+        properties : {
+            lineHeight : '32px'
+        }
+    });
+
+    // create a search input field for the footer
+    var search_input = new Paper({
+        depth : 20,
+        type : Paper.TYPE_TILE,
+        backing : new InputSurface({
+            placeholder : 'Search doesn\'t search, but whatever'
+        })
+    });
+
+    // make the button emit the ripple/touch effect
+    search_button.mixin(TouchEffect);
+
+    // make the button actually look like a button
+    search_button.mixin(ButtonEffect, {
+        type : ButtonEffect.TYPE_RAISED
+    });
+
+    // clear the search input on click
+    search_button.on('click', function() {
+        search_input.getBacking().setValue('');
+    });
+
+    // create the footer from its components
+    var footer = new LayoutController({
+        layout : NavBarLayout,
+        layoutOptions : {
+            margins : 0,
+            itemSpacer : 0
+        },
+        dataSource : {
+            title : search_input,
+            leftItems : [search_button]
         }
     });
 
@@ -41,11 +81,6 @@ define(function(require, exports, module) {
         depth : 30,
         size : [48, 48],
         content : '+'
-    });
-
-    // do something (or nothing) on click
-    button.on('click', function() {
-
     });
 
     // make the button emit the ripple/touch effect
@@ -64,6 +99,7 @@ define(function(require, exports, module) {
     })).add(button);
 
     context.add(new Modifier({
+        size : [undefined, 32],
         origin : [0.0, 1.0],
         align : [0.0, 1.0]
     })).add(footer);
